366e0486625d2cc82bcc4d0e68e6ad2bdd423846
Aric Walker <aricwalker@github.com>
2017-11-25 19:11:41 -0700

    Working setup, squashed initial commit
    Updated .gitignore

---
+++ spec/project-icons-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ProjectIconsView from '../lib/project-icons-view';
+
+describe('ProjectIconsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
